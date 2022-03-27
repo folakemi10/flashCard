145d43f9311c3a582f6232bf69a14a2e24e7a34f
folakemi10 <f.shofu@wustl.edu>
2022-03-27 04:26:17 -0500

    lab 3 updates

--- app/src/main/java/com/example/flashcard/MainActivity.java
+++ app/src/main/java/com/example/flashcard/MainActivity.java
@@ -7,15 +7,24 @@ package com.example.flashcard;
         import android.view.View;
         import android.widget.TextView;
 
+        import com.google.android.material.snackbar.Snackbar;
+
+        import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    TextView questionText;
+    TextView answerText;
+    FlashcardDatabase flashcardDatabase;
+    List<Flashcard> allFlashcards;
+    int cardIndex =0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView questionText = findViewById(R.id.flashcard_q_textview);
-        TextView answerText = findViewById(R.id.flashcard_a_textview);
+        questionText = findViewById(R.id.flashcard_q_textview);
+        answerText = findViewById(R.id.flashcard_a_textview);
         questionText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -38,8 +47,52 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        //in oncreate because after oncreate we have context to give the constructor
+        flashcardDatabase = new FlashcardDatabase(this);
+        allFlashcards = flashcardDatabase.getAllCards();
 
+        if(allFlashcards != null && allFlashcards.size() > 0) {
+            Flashcard firstCard = allFlashcards.get(0);
+            questionText.setText(allFlashcards.get(0).getQuestion());
+            answerText.setText(allFlashcards.get(0).getAnswer());
+//            questionText.setText(firstCard.getQuestion());
+//            answerText.setText(firstCard.getQuestion());
+        }
 
+        findViewById(R.id.nextButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (allFlashcards == null || allFlashcards.size() == 0){
+                    return;
+                }
+                cardIndex +=1;
+               if(cardIndex >= allFlashcards.size()) {
+                   Snackbar.make(view, "you've reached the end of cards.",Snackbar.LENGTH_SHORT).show();
+                cardIndex = 0;
+               }
+                Flashcard currentCard = allFlashcards.get(cardIndex);
+                questionText.setText(currentCard.getQuestion());
+                answerText.setText(currentCard.getAnswer());
+            }
+        });
+        findViewById(R.id.deleteButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                flashcardDatabase.deleteCard(questionText.getText().toString());
+                allFlashcards = flashcardDatabase.getAllCards();
+                if (allFlashcards == null || allFlashcards.size() == 0) {
+                    Intent intent = new Intent(MainActivity.this, Empty.class);
+                    MainActivity.this.startActivity(intent);
+                }
+                else {
+                    cardIndex -= 1;
+                    Flashcard currentCard = allFlashcards.get(cardIndex);
+                    questionText.setText(currentCard.getQuestion());
+                    answerText.setText(currentCard.getAnswer());
+                }
+                }
+            });
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -47,8 +100,13 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == 100 && resultCode == RESULT_OK) { // this 100 needs to match the 100 we used when we called startActivityForResult!
             String newQuestion = data.getExtras().getString("q"); // 'string1' needs to match the key we used when we put the string in the Intent
             String newAnswer = data.getExtras().getString("a");
-            ((TextView) findViewById(R.id.flashcard_q_textview)).setText(newQuestion);
-            ((TextView) findViewById(R.id.flashcard_a_textview)).setText(newAnswer);
+            questionText.setText(newQuestion);
+            answerText.setText(newAnswer);
+
+            Flashcard flashcard = new Flashcard(newQuestion,newAnswer);
+            flashcardDatabase.insertCard(flashcard);
+
+            allFlashcards = flashcardDatabase.getAllCards();
         }
     }
 }
